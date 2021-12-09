8b9dff94363228d78406fc36acbd852e99090476
Gabriel Pelizario <gabrielpelizario2013@gmail.com>
2021-12-09 19:28:55 -0300

    criamos a classe Page

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <?php 
 
+//faz parte do composer para traser as dependencias 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+//são namespaces do vendor
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
+//o que está acima é sempre o que vamos precisar para criar nossas paginas
 $app->get('/', function() {
     
-	$sql = new Hcode\DB\sql();
+    //neste momento vai chamar o construct e vai chamar o header na sua tela
+	$page = new page();
 
-	$results = $sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
+	$page->setTpl("index"); 
 
 });
 
