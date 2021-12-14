ae9ea3ee838705348cdcd14445a2eb02e5bfd8c3
Gabriel Pelizario <gabrielpelizario2013@gmail.com>
2021-12-14 18:42:22 -0300

    criando o page admin e o tpl admin

--- index.php
+++ index.php
@@ -6,16 +6,29 @@ require_once("vendor/autoload.php");
 //são namespaces do vendor
 use \Slim\Slim;
 use \Hcode\Page;
+use Hcode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
+//rota para a pagina da web (usuário)
 //o que está acima é sempre o que vamos precisar para criar nossas paginas
 $app->get('/', function() {
     
     //neste momento vai chamar o construct e vai chamar o header na sua tela
 	$page = new page();
+
+	$page->setTpl("index"); 
+
+});
+
+//rota para a pagina de admin
+//o que está acima é sempre o que vamos precisar para criar nossas paginas
+$app->get('/admin', function() {
+    
+    //neste momento vai chamar o construct e vai chamar o header na sua tela
+	$page = new PageAdmin();
 
 	$page->setTpl("index"); 
 
