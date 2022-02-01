b80a74c1d87f29fb80dd23751b46ea0bf0fe0f44
Gabriel Pelizario <gabrielpelizario2013@gmail.com>
2022-02-01 19:56:09 -0300

    Detalhes do Produto

--- site.php
+++ site.php
@@ -49,3 +49,19 @@ $app->get("/categories/:idcategory", function($idcategory){
 	]); 
 
 });
+
+//Rota para a pagina de detalhes do produto
+$app->get("/products/:desurl", function($desurl){
+
+	$product = new Product();
+
+	$product->getFromURL($desurl);
+
+	$page = new Page();
+
+	$page->setTpl("product-detail", [
+		'product'=>$product->getValues(),
+		'categories'=>$product->getCategories()
+	]);
+
+});
